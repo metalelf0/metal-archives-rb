5e040a69cc380e6bee945a1ecc7e0171e6d21e30
Metalelf0 <metalelf0@gmail.com>
2010-01-21 08:16:16 +0100

    Removed un-needed step definitions

--- features/step-definitions.rb
+++ features/step-definitions.rb
@@ -15,28 +15,5 @@ end
 
 Then /^it should have the following albums:$/ do |expected_table|
   actual_table = @band_data_retriever.band.albums.map {|album| [album.title, album.year.to_s, album.rating.to_s ] }
-  expected_table.diff!(actual_table)    
-  
-end
-
-
-Then /^it should have an album with index "([^\"]*)"$/ do |index|
-  @index = index.to_i
-  @band_data_retriever.band.albums[@index].should_not be_nil
-end
-
-Then /^its title should be "([^\"]*)"$/ do |title|
-  @band_data_retriever.band.albums[@index].title.should eql(title)
-end
-
-Then /^its year should be "([^\"]*)"$/ do |year|
-  @band_data_retriever.band.albums[@index].year.should eql(year.to_i)
-end
-
-Then /^its rating should be "([^\"]*)"$/ do |rating|
-  if rating == ""
-    @band_data_retriever.band.albums[@index].rating.should be_nil
-  else
-    @band_data_retriever.band.albums[@index].rating.should eql(rating.to_i)
-  end
+  expected_table.diff!(actual_table)
 end
